5b6aa6a0d799efb3b9b0899b272fee59113b2572
Acchu <81754494+Acchu07@users.noreply.github.com>
2025-05-16 21:00:42 +0530

    'Hello World' TS-Express App
    Folder Structure
    Setup TS with Express

---
+++ src/scripts/tempFetchTest.ts
@@ -0,0 +1,13 @@
+const url = 'https://api.coingecko.com/api/v3/ping';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    'x-cg-demo-api-key': process.env.API_KEY as string
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
